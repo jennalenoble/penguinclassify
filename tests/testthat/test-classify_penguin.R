d4dc2005af0dcca1189239e43680a93351a0a2bc
Jenna Le Noble <jenna_lenoble@hotmail.com>
2023-03-28 15:16:15 -0700

    wrote tests for classify penguin

---
+++ tests/testthat/test-classify_penguin.R
@@ -0,0 +1,7 @@
+test_that("Penguin classification works", {
+  expect_equal(classify_penguin(50, 100), "Chinstrap")
+})
+
+test_that("function input", {
+  expect_error(classify_penguin(NA, "100"), "missing value where TRUE/FALSE")
+})
